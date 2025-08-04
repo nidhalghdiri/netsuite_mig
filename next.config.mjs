32841b9295f42092bfa90e17d99746f4333da3e3
Nidhal Ghdiri <ghdiri.nidhal@gmail.com>
2025-08-04 09:40:25 +0400

    Setup Project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/netsuite",
+  output: "standalone", // Recommended for production
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "/:path*",
+      },
+      // Handle API routes
+      {
+        source: "/api/:path*",
+        destination: "/api/:path*",
+      },
+    ];
+  },
+};
 
 export default nextConfig;
