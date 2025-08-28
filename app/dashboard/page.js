7bdd456702a9ef9c6bbe2e4fb1a3637fd88bd351
Nidhal Ghdiri <ghdiri.nidhal@gmail.com>
2025-08-28 20:05:52 +0400

    Handle Inventory ERROR when create Invoice 5

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -840,7 +840,7 @@ export default function DashboardOverview() {
       });
 
       var invAdjustData = {
-        externalId: "",
+        externalId: `IANEW-${itemId}-${assignmentId}`,
         tranId: `IANEW-${itemId}-${assignmentId}`,
         tranDate: transactionData.tranDate,
         memo: `معالجة مخزون الصنف ${itemId} \n رقم الفاتورة ${transactionData.tranId} \n رقم التاكيد ${assignmentId} \n بكمية ${shortfall}`,
