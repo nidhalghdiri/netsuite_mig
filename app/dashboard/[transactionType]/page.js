5c9a8c4c776e9434dcaa93ae160030ac0509b6ef
Nidhal Ghdiri <ghdiri.nidhal@gmail.com>
2025-09-24 17:09:31 +0400

    Add transactions Page Comparision 11

--- app/dashboard/[transactionType]/page.js
+++ app/dashboard/[transactionType]/page.js
@@ -28,7 +28,7 @@ export default function TransactionTypePage() {
 
   // Map URL transaction types to NetSuite internal types
   const typeMapping = {
-    invoice: "CustInvc",
+    invoices: "CustInvc",
     "inventory-adjustment": "InvAdjst",
     "transfer-order": "TrnfrOrd",
     "inventory-transfer": "InvTrnfr",
